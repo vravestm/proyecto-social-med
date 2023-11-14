7be7a933d456b6155bf52e3e8bf9d647c1f9d2c4
stian00 <sebastian.moreira04@inacapmail.cl>
2023-11-14 14:28:44 -0300

    update1
    añadiendo el modelo de los usuarios a los modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User #aca se importa el username interno de django
+from datetime import datetime #aca se importa la fecha para los datefield de los modelos
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    username = models.ForeignKey(User,verbose_name="username",on_delete=models.CASCADE,null=False)
+    nombre = models.CharField(max_length=20,verbose_name="Nombre",null=False)
+    apellido1 = models.CharField(max_length=20,verbose_name="Primer Apellido",null=False)
+    apellido2 = models.CharField(max_length=20,verbose_name="Segundo Apellido",null=False)
+    edad = models.IntegerField(max_length=3,verbose_name="Edad",null=False)
+    zona1 = models.CharField(max_length=20,verbose_name="Comunidad Autonoma",null=False)
+    zona2 = models.CharField(max_length=20,verbose_name="Provincia",null=False)
+    zona3 = models.CharField(max_length=20,verbose_name="Municipio",null=False)
+    correo = models.EmailField(max_length=30,verbose_name="Direccion de correo",null=False)
+    telefono = models.CharField(max_length=20,verbose_name="Numero Telefonico",null=False)
+    imagen = models.ImageField(upload_to='media/',verbose_name="Imagen", null=True)
+
+
+    def __str__(self):
+        return self.username
+    class Meta:
+        verbose_name="Usuario"
+        verbose_name_plural="Usuarios"
+
+
+
