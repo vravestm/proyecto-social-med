2c371d67ad00de9acddcc43dfd31c01cd7a13b7f
stian00 <sebastian.moreira04@inacapmail.cl>
2023-11-29 20:47:17 -0300

    cambios en admin.py

--- cursos/admin.py
+++ cursos/admin.py
@@ -3,4 +3,8 @@ from .models import Cursos
 from .models import Inscripcion
 
 admin.site.register(Cursos)
-admin.site.register(Inscripcion)
+
+class Inscripcionadmin(admin.ModelAdmin):
+      list_display = ('curso', 'user','date','id')
+      list_filter = ('curso', 'user')
+admin.site.register(Inscripcion,Inscripcionadmin)
