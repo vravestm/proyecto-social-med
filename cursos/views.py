98858a94ed0d278123325fc030b883ed47498279
stian00 <sebastian.moreira04@inacapmail.cl>
2023-11-30 22:45:45 -0300

    pequeñas validaciones

--- cursos/views.py
+++ cursos/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from .models import Cursos
 from .models import Inscripcion
+from django.contrib.auth.decorators import login_required
 
 
 def cursos(request):
-    request.session['usuario']=request.user.id 
+    if request.user.is_authenticated:
+        request.session['usuario']=request.user.id 
     cur=Cursos.objects.all()
     return render(request, 'core/cursos.html',{'cur':cur})
 
@@ -14,7 +16,7 @@ class Detalle(DetailView): #Esta clase permite que se vea el detalle del curso p
     model = Cursos
     template_name = 'core/informacion_curso.html'
 
-
+@login_required(login_url='inicio_sesion')
 def confirmacion(request): #Confirmacion para inscribirse dentro del curso
 
     a = request.GET.get('valor')
