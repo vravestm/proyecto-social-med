dc6ff1ba6f943db8a2bd4c4802c7801b7ac5fda6
stian00 <sebastian.moreira04@inacapmail.cl>
2023-12-06 17:57:41 -0300

    agregada la funcionalidades respecto de talleres (hola yene)

--- talleres/views.py
+++ talleres/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Talleres
+from django.views.generic import DetailView
 
 
 def talleres(request):
-    return render(request, 'core/talleres.html')
+    if request.user.is_authenticated:
+        request.session['usuario']=request.user.id
+    tal=Talleres.objects.all()
+    return render(request, 'core/talleres.html',{'tal':tal})
+
+class Detalletaller(DetailView):
+    model = Talleres
+    template_name = 'core/informacion_taller.html'
